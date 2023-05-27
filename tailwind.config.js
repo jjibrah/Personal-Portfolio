163d3815acda231cd1a33768eefe70de9e825d23
jjibrah <ibrabdi109@gmail.com>
2023-05-28 00:44:57 +0300

    edited the id of the reply email input

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,6 @@ module.exports = {
   content: ["./build/*.html","./build/js/*.js"],
   theme: {
     extend: {
-      colors:{
-        papayawhip: {
-          light: '#fef4e4',
-          DEFAULT:'#ffefd5',
-          dark: 'fee5bc'
-        }
-      },
       screens: {
         'widescreen': { 'raw': '(min-aspect-ratio: 3/2)' },
         'tallscreen': { 'raw': '(maxn-aspect-ratio: 13/20)' },
